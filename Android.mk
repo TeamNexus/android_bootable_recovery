4721cf44e11be755d4baeeaf9f95f0da5f13ca22
Lukas0610 <mail@lukasberger.at>
2018-06-16 17:55:50 +0200

    fixup TWRP for NexusOS-integration
    Change-Id: If1011192e9c8009843d4888a9d2b15b4d1ec9c67

--- Android.mk
+++ Android.mk
@@ -14,6 +14,9 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# use toybox instead of busybox when building TWRP
+TW_USE_TOOLBOX := true
+
 ifdef project-path-for
     ifeq ($(LOCAL_PATH),$(call project-path-for,recovery))
         PROJECT_PATH_AGREES := true
