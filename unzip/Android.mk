58220b477dbb127e66cacb213522975d6954a0b4
Lukas0610 <mail@lukasberger.at>
2018-06-27 12:46:32 +0200

    unzip: fix invalid linker path

--- unzip/Android.mk
+++ unzip/Android.mk
@@ -24,10 +24,21 @@ LOCAL_SRC_FILES := \
 	process.c \
 	unix/unix.c
 
+LOCAL_LDFLAGS :=
 LOCAL_CFLAGS := -DACORN_FTYPE_NFS -DWILD_STOP_AT_DIR -DLARGE_FILE_SUPPORT \
 	-DUNICODE_SUPPORT -DUNICODE_WCHAR -DUTF8_MAYBE_NATIVE -DNO_LCHMOD \
 	-DDATE_FORMAT=DF_YMD -DUSE_BZIP2 -DIZ_HAVE_UXUIDGID -DNOMEMCPY \
 	-DNO_WORKING_ISPRINT -DNO_LANGINFO_H -Wno-format-security
+
+ifneq ($(TARGET_ARCH), arm64)
+    ifneq ($(TARGET_ARCH), x86_64)
+        LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker
+    else
+        LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker64
+    endif
+else
+    LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker64
+endif
 
 LOCAL_C_INCLUDES := \
 	external/bzip2/include
